4ac89fbaaef55a4a163a36572ccbaa36a9f0acab
Théo <t.baseilhac@gmail.com>
2016-09-29 23:52:56 +0200

    Added unit tests for tokenization and stemming, reorganized files

--- run.py
+++ run.py
@@ -1,73 +1,68 @@
 import glob
-import string
-import nltk
-
-
-def tokenize(text):
-    text = text.lower().translate(str.maketrans("", "", string.punctuation))
-    tokens = nltk.word_tokenize(text)
-    return tokens
+import preprocessing
 
 # //////////////////////////////////////////////////////////////////////////////
 
-voc_dict = {}
+def run():
 
-#the schema of the posting_file is : (word,[postingList])
-posting_file = []
+    voc_dict = {}
 
-for filename in glob.glob('./latimes/la*'):
-    doc_id = filename[2:]
+    #the schema of the posting_file is : (word,[postingList])
+    posting_file = []
 
-    with open(filename, 'r') as f:
-        for line in f:
+    for filename in glob.glob('./latimes/la*'):
+        doc_id = filename[2:]
 
-            # tokenization
-            tokens = tokenize(line)
+        with open(filename, 'r') as f:
+            for line in f:
 
-            # stemming
+                # tokenization
+                tokens = tokenize(line)
 
-            # stop words removal
+                # stemming
 
-            for word in tokens:
-                #if the word does not exists
-                if word not in voc_dict:
-                    #we must find the right place to put our new line corresponding to a word
+                # stop words removal
 
-                    index_insertion = 0
+                for word in tokens:
 
-                    #TODO: we can make an dichotomy for more perfs
-                    for i in range(len(posting_file)):
-                        key_word = posting_file[0][0]
-                        index_insertion = i
-                        if word > key_word:
-                            #we leave the loop
-                            break
+                    #if the word does not exist
+                    if word not in voc_dict:
+                        #we must find the right place to put our new line corresponding to a word
 
-                    voc_dict[word] = [1, index_insertion]
+                        index_insertion = 0
 
-                    #we add to the right position
-                    posting_file.insert(index_insertion, (word,[filename, 1]))
+                        #TODO: we can make a dichotomy for better perf
 
-                #the word already exists (there is a line in posting_file)
-                else:
+                        for i in range(len(posting_file)):
+                            key_word = posting_file[0][0]
+                            index_insertion = i
+                            if word > key_word:
+                                #we leave the loop
+                                break
 
-                    #we add to the right place
-                    #TODO dichotomy !
+                        voc_dict[word] = [1, index_insertion]
 
-                    index_insertion = 0
-                    insert = False
-                    for i in range(len(posting_file[word][1])):
-                        index_insertion = i
+                        #we add to the right position
+                        posting_file.insert(index_insertion, (word,[filename, 1]))
 
-                        #if the word is already present in this document, we increment
-                        if filename == posting_file[word][1][i]:
-                            posting_file[word][1][i][1] += 1
-                            break
-                        elif filename > posting_file[word][1][i]:
-                            insert = True
-                            break
+                    #the word already exists (there is a line in posting_file)
+                    else:
 
-                    if insert:
-                        posting_file[word][1].insert(index_insertion, [filename, 1])
+                        #we add to the right place
+                        #TODO dichotomy !
 
+                        index_insertion = 0
+                        insert = False
+                        for i in range(len(posting_file[word][1])):
+                            index_insertion = i
 
+                            #if the word is already present in this document, we increment
+                            if filename == posting_file[word][1][i]:
+                                posting_file[word][1][i][1] += 1
+                                break
+                            elif filename > posting_file[word][1][i]:
+                                insert = True
+                                break
+
+                        if insert:
+                            posting_file[word][1].insert(index_insertion, [filename, 1])
