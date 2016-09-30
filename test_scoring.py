7accaddf46dfb45ceefbfd0756be7d6fc3733e39
Théo <t.baseilhac@gmail.com>
2016-09-30 08:51:24 +0200

    Corrected test for posting list score calculation to take idf into account

--- test_scoring.py
+++ test_scoring.py
@@ -1,10 +1,11 @@
+import math
 import scoring
 
 def test_calculatePostingListScore():
-    postingList = [["a.doc", 3], ["b.doc", 2], ["c.doc", 1], ["d.doc", 7]]
-    docsTokenCounts = {"a.doc": 20, "b.doc": 20, "c.doc": 20, "d.doc": 20}
+    #TODO: write correct unit test with idf scoring
+    postingList = [["a.doc", 180], ["b.doc", 200], ["c.doc", 37]]
+    docsTokenCounts = {"a.doc": 200, "b.doc": 400, "c.doc": 200, "d.doc": 40}
     postingList = scoring.calculatePostingListScore(postingList, docsTokenCounts)
-    assert(postingList[0][1]==(3/20))
-    assert(postingList[1][1]==(2/20))
-    assert(postingList[2][1]==(1/20))
-    assert(postingList[3][1]==(7/20))
+    assert(postingList[0][1]==(180/200)*math.log(4/3))
+    assert(postingList[1][1]==(200/400)*math.log(4/3))
+    assert(postingList[2][1]==(37/200)*math.log(4/3))
