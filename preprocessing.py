32eae158d9387530646a306721f8d88e92f6c6a4
Théo <t.baseilhac@gmail.com>
2016-09-30 17:18:01 +0200

    Added stemming function & unit test - Corrected tokenization unit test

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import string
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem.porter import *
 
 #########################
 # Preprocessing functions
@@ -14,8 +15,14 @@ def tokenize(text):
 
 # Returns a stemmed version of the given word_tokenize
 def stem(word):
-    #TODO : todo
-    return word
+    stemmer = PorterStemmer()
+    return stemmer.stem(word)
+
+def isConsonant(letter):
+    return not letter in "aeiouy"
+
+def isVowel(letter):
+    return letter in "aeiouy"
 
 def removeStopWords(tokens):
     filtered_words = [word for word in tokens if word not in stopwords.words('english')]
--- test_preprocessing.py
+++ test_preprocessing.py
@@ -6,16 +6,19 @@ import preprocessing
 #########################
 
 def test_tokenization():
-    toTokenize = "\"Text to tokenize, i.e. untokenized text, has to be tokenized. It has to be tokenized as soon as possible\""
-    tokens = ["text", "to", "tokenize", "i.e.", "untokenized", "text", "has", "to", "be", "tokenized", "it", "has", "to", "be", "tokenized", "as", "soon", "as", "possible"]
+    toTokenize = "\"Text to tokenize (in fact, untokenized text), has to be tokenized. It has to be tokenized as soon as possible\""
+    tokens = ["text", "to", "tokenize", "in", "fact", "untokenized", "text", "has", "to", "be", "tokenized", "it", "has", "to", "be", "tokenized", "as", "soon", "as", "possible"]
     tokensToTest = preprocessing.tokenize(toTokenize)
     for a, b in zip(tokens, tokensToTest):
         assert(a == b)
 
 def test_stemming():
-    assert(preprocessing.stem("tokenized") == "token")
-    assert(preprocessing.stem("stemmer") == "stem")
-    assert(preprocessing.stem("algorithmic") == "algorithm")
+    wordsToStem = ["tokenized", "roses", "brutally", "dozens", "stupidity", "notorious", "english", "stemming", "stemmed", "soft"]
+    for i in range(0, len(wordsToStem)-1):
+        wordsToStem[i] = preprocessing.stem(wordsToStem[i])
+    stemmedWords = ["token", "rose", "brutal", "dozen", "stupid", "notori", "english", "stem", "stem", "soft"]
+    for a, b in zip(wordsToStem, stemmedWords):
+        assert(a == b)
 
 def test_removeStopWords():
     tokensToFilter = ["In", "this", "text", "stop", "words", "have", "to", "be", "removed", "In", "each", "phrase", "occurances", "of", "words", "such", "as", "this", "that", "so", "and", "and", "of", "will", "be", "removed"]
