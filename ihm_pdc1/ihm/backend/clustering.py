ffa91cc3e52625dc9f24dff44c9b7457784fc5ee
Côme CACHARD <comecachard@yahoo.fr>
2016-10-27 18:17:24 +0200

    creation of method 'clustering_process_for_best_results_query' which do the trick for clustering and returns two dict, one for the clusters and doc_id, the other for the describing words of cluster ; for the two the key is the id of the cluster

--- ihm_pdc1/ihm/backend/clustering.py
+++ ihm_pdc1/ihm/backend/clustering.py
@@ -25,11 +25,6 @@ def calculate_cosine_matrix(terms_dict_doc_list):
     return matrix
 
 
-def get_terms_dict_for_doc(doc_id,doc_dir):
-    #return a dict where key is term and value is occurence for a doc referenced by doc_id
-
-    print("begin")
-
 
 def calculate_norm_of_document_vector(document_vector):
     #calculate and return the norm of a document vector represented by a list.
@@ -116,8 +111,71 @@ def get_description_of_cluster(dict_list,number_of_keys_to_considered=3):
 
 
 
+def clustering_process_for_best_results_query(voc, query_results_list, number_of_keys_to_considered=3, number_of_clusters_wanted=3, number_of_describing_words=3):
+# this method take in parameters an object vocabulary 'voc', and a list of results of a query which is a list of [docId, score]
+# two optional parameters, 'number_of_keys_to_considered' for the number of words we look in a doc's terms_dict
+#                          'number_of_clusters_wanted' for the number of clusters to create
+#                          'number_of_describing_words' for the number of words for describing a cluster
+# this method will create clusters and for each cluster see the best words that describe the cluster
+# it returns a dict where key is the number of cluster and value is a list that contains doc_id, and an other dict where key is the number of cluster and value is a list taht contains describing words
+    
+    #first we get the dict for all docs where key is docId and value is an other dict where key is term and value is score
+    terms_dicts_for_docs_dic = voc.get_terms_dicts_for_docs()
+
+    # we will get the term_dict for the doc_ids in query_results_list
+    fagin_result_doc_list = list()
+
+    #so we get the term_dict with the doc_id
+    for item in query_results_list:
+        fagin_result_doc_list.append(terms_dicts_for_docs_dic[item[0]])#item[0] is docId
+
+
+    #Then we will calculate cosine similarity with the docs's terms dicts where key is term and value is score 
+    #we calculate the cosine matrix
+    matrix = calculate_cosine_matrix(fagin_result_doc_list)
+
+    #with the cosine matrix, we can find clusters
+    doc_id_clusters = get_k_clusters_from_matrix(number_of_clusters_wanted,matrix)
+    print(doc_id_clusters)
+
+    #we create a dictionary where key is the number of cluster and value is a list of words that describes this cluster
+    describing_words_clusters_lists_dict = dict()
+
+    # we create a dict where key is the number of the cluster and value is a list that contains the doc_ids
+    doc_id_clusters_dict = dict()
+
+    #then we go through each cluster in order to see which words best describe them
+    i=0
+    for id_cluster in doc_id_clusters:
+        print("cluster ",i," : ")
+        i +=1
+
+        doc_id_clusters_dict[i] = id_cluster
+
+        dict_cluster_list = list()
+
+        for doc_id in id_cluster:
+            print(doc_id)
+            dict_cluster_list.append(fagin_result_doc_list[doc_id])
+
+        description = get_description_of_cluster(dict_cluster_list,number_of_keys_to_considered)
+
+        describing_words_clusters_lists[i] = description
+
+        for j in range(number_of_describing_words):
+            print(description[j])
+
+        print("____________________________")
+
+    return (doc_id_clusters_dict,describing_words_clusters_lists_dict)
+
+
+
+
+
 def test():
 
+    
     dico0 = {"insa":10,"informatique":3,"ordinateur":5,"cours":7,"sport":2}
     #norm1 = calculate_norm_of_document_vector(dico1.values())
     #print("norm1 = ",norm1)
